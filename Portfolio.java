2b79d224fde4b6c4d0b66b4c300f7b84e2c0a1a1
JonLeFrois <jp29lefr@siena.edu>
2023-02-23 14:27:23 -0500

    Added banner and method comments

--- Portfolio.java
+++ Portfolio.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 /**
- * Write a description of class Portfolio here.
+ * Holds an ArrayList with the stock portfolio and keeps track of
+ * lifetime investment and payout as well as buying and selling stocks
  *
  * @author Jon LeFrois
  * @version 2/23/23
@@ -11,20 +12,40 @@ public class Portfolio
     private double lifetimeInvestment;
     private double lifetimePayout;
     
+    /**
+     * Constructor for Portfolio
+     * Initializes ArrayList and sets investment and payout to 0
+     */
     public Portfolio(){
         stocks = new ArrayList<StockHolding>();
         lifetimeInvestment = 0;
         lifetimePayout = 0;
     }
     
+    /**
+     * Returns total lifetime investment
+     * 
+     * @return  the total lifetime investment
+     */
     public double getLifetimeInvestment(){
         return lifetimeInvestment;
     }
     
+    /**
+     * Returns total lifetime payout
+     * 
+     * @return  the total lifetime payout
+     */
     public double getLifetimePayout(){
         return lifetimePayout;
     }
     
+    /**
+     * If the stock is already in the portfolio, buys the inputted amount,
+     * if not, it adds the new stock to the portfolio
+     * 
+     * @return  the cost of buying the stock
+     */
     public double buyStock(String symbol, String name, int numToBuy, double price){
         for(int i = 0; i < stocks.size(); i++){
             if(stocks.get(i).getSymbol().equals(symbol)){
@@ -37,6 +58,12 @@ public class Portfolio
         return numToBuy * price;
     }
     
+    /**
+     * Returns the index of the inputted stock symbol if it is in the
+     * portfolio, -1 otherwise
+     * 
+     * @return  the index of the inputted stock, -1 otherwise
+     */
     private int getIndex(String symbol){
         for(int i = 0; i < stocks.size(); i++){
             if(stocks.get(i).getSymbol().equals(symbol)){
@@ -46,6 +73,12 @@ public class Portfolio
         return -1;
     }
     
+    /**
+     * Sells the inputted amount of the inputted stock if it is in the
+     * portfolio and returns the payout
+     * 
+     * @return  the payout from selling the stock, or 0 if not sold
+     */
     public double sellStock(String symbol, int numToSell){
         for(int i = 0; i < stocks.size(); i++){
             if(stocks.get(i).getSymbol().equals(symbol)){
@@ -61,6 +94,11 @@ public class Portfolio
         return 0;
     }
     
+    /**
+     * Returns the current total value of the portfolio
+     * 
+     * @return  the current total value of the portfolio
+     */
     public double getCurrentValue(){
         double value = 0;
         for(int i = 0; i < stocks.size(); i++){
@@ -69,6 +107,11 @@ public class Portfolio
         return value;
     }
     
+    /**
+     * Returns the formatted string for the portfolio with its information
+     * 
+     * @return  formatted string for the portfolio and its information
+     */
     @Override
     public String toString()
     {
